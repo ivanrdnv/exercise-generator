a9434a9056f85989fa5684c0936585d4473181a9
Ivan Rudnev <ivzavertkin@gmail.com>
2023-07-05 16:50:29 +0300

    Reordered files and folders. Added basic code to app.py. Create a basic functionallity for the text parsing in notebook.

---
+++ app/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+# import joblib 
+
+st.header('Генератор упражнений по английскому')
+st.subheader('Вставьте текст для создания упражнения')
