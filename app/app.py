1f42e007c938c436e01cf3696f7e4906e8d64978
Ivan Rudnev <ivzavertkin@gmail.com>
2023-07-12 16:09:57 +0300

    1. "Add basic functionality to the app" 2.  "Initial implementation of core features" 3. "Create basic structure for the app" 4. "Set up app with basic functionality" 5. "Implement minimum viable product (MVP) functionality"

--- app/app.py
+++ app/app.py
@@ -1,28 +1,93 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from sentence_splitter import SentenceSplitter
+import random
+import text_utils 
+from streamlit_sortables import sort_items
+from annotated_text import annotated_text
+
+options_pos = ["______", "verb", "noun", "adjective", "pronoun", "adverb"]
+
+@st.cache_data
+def process_text_file(text):
+    df = text_utils.text_to_dataframe(text)
+    return df
  
-st.header('Exercise Generator for English')
+def main():
+    st.title('Exercise Generator')
 
-data = st.file_uploader("Upload your text", type=["txt"])
-if data is not None:
-    text = data.getvalue().decode('utf-8')
-    df = text_to_sentences(text)
+    st.header('Upload Text File')
+    text_file = st.file_uploader('Choose a text file', type=['txt'])
+    # TODO: 1. rewrite the logic 
+    # TODO: 2. add the improved function missing_word
+    if text_file is not None:
+        text = text_file.read().decode('utf-8')
+        df = process_text_file(text)
 
-    df_filtered = df[df['raw'].apply(lambda x: len(x.split()) > 5)]   
-    df_filtered['select_word'] = df_filtered['raw'].apply(select_word)
-    st.dataframe(df_filtered.head(15))
-    #st.text(text)
-    
+        for index, row in df.iterrows():
+            exercise_type = row['exercise']
+            sentence = row['raw']
+
+            if exercise_type is None:
+                st.write(sentence)
+
+            elif exercise_type == 'select_word':
+                selected_word = row['correct_answer']
+                options = row['options']
+
+                col_sentence, col_word = st.columns([3, 1])
+                sentence = sentence.replace(selected_word, '**{}**')
+
+                with col_word:
+                    user_answer = None
+                    user_answer = st.selectbox("Select the missing word:", options, index=options.index('______'), key=index)
+
+                with col_sentence:
+                    st.write(sentence.format(user_answer))
+
+                    if user_answer == '______':
+                        pass
+                    else:
+                        if user_answer == selected_word:
+                            st.success("Correct!")
+                        else:
+                            st.error("Incorrect!")
+
+            elif exercise_type == 'arrange_words':
+                correct_answer = row['correct_answer']
+                words = row['options']
+
+                st.caption('Put words in the correct order:')
+                user_answer = sort_items(words)
+                user_answer = ' '.join(word.strip() for word in user_answer)
+
+                
+                if  user_answer== correct_answer:
+                        st.success("Correct!")
+               
+
+            elif exercise_type == 'word_category':
+                chosen_word = row['options']
+                correct_category = row['correct_answer']
+                sentence = sentence.split(chosen_word)
+
+                col_sentence_pos, col_word_pos = st.columns([3, 1])
+                with col_word_pos:
+                    user_category = st.selectbox("Select the category of the word:", options_pos, index=0, key=index)
+                with col_sentence_pos:
+                    annotated_text(sentence[0], (chosen_word, user_category), sentence[1])
+                    if user_category == '______':
+                        pass
+                    else:
+                        if user_category == correct_category:
+                            st.success("Correct!")
+                        else:
+                            st.error("Incorrect!")
+
+                
+
+if __name__ == "__main__":
+    main()
 
 
 
-st.subheader('Select the correct options for the missing words:')
-
-sentence = "Once upon a time there {} a sweet little girl."
-options = ['was', 'were', 'be', 'is', 'select']
-
-missing_word = st.selectbox("Select the missing word:", options)
-exercise_sentence = sentence.format(missing_word)
-
-st.write(exercise_sentence)
--- app/text_utils.py
+++ app/text_utils.py
@@ -5,12 +5,25 @@ from sentence_splitter import SentenceSplitter
 import random
 from pyinflect import getAllInflections
 
+exercise_percentage = 0.5
+min_sentence_length = 5
+max_sentence_length = 15
+
+# word categories
+categories = {
+        "noun": ["NOUN", "PROPN"],
+        "adjective": ["ADJ"],
+        "verb": ["VERB"],
+        "adverb": ["ADV"],
+        "pronoun": ["PRON"]
+    }
+
 nlp = spacy.load('en_core_web_sm')
 
 # Ogden's Basic English Word List (850)
-ogden_basic_english = pd.read_csv('ogden_basic_english.csv')['0'].values.tolist()
+# ogden_basic_english = pd.read_csv('ogden_basic_english.csv')['0'].values.tolist()
 
-def text_to_sentences(text):
+def text_to_dataframe(text):
     """
     Transforms the provided text into a pandas DataFrame containing sentences.
     Parameters: text (str). Returns: pandas.DataFrame.
@@ -18,7 +31,28 @@ def text_to_sentences(text):
     splitter = SentenceSplitter(language='en')
     sentences = splitter.split(text)
     df = pd.DataFrame(sentences, columns=['raw'])
+    df['exercise'] = df['raw'].apply(lambda sentence: random.choice(list(exercise_functions.keys()))
+                                     if min_sentence_length < len(nlp(sentence)) < max_sentence_length 
+                                     and random.random() < exercise_percentage else None)
+
+    def update_row(row):
+        exercise_type = row['exercise']
+        sentence = row['raw']
+        if exercise_type in exercise_functions:
+            result = exercise_functions[exercise_type](sentence)
+            if pd.isna(result):
+                row['exercise'] = None
+            else:
+                correct_answer, options = result
+                row['correct_answer'] = correct_answer
+                row['options'] = options
+        return row
+
+    df = df.apply(update_row, axis=1)
+
     return df
+
+
 
 def select_word(sentence):
     """
@@ -27,14 +61,15 @@ def select_word(sentence):
     """
     doc = nlp(sentence)
     candidate_tokens = [token for token in doc if not token.is_punct and not token.is_space
+                            and not token.is_digit
                             and token.is_lower and token.tag_.startswith(('V', 'N'))] # , 'A'
     if not candidate_tokens:
-        return np.nan, np.nan
+        return np.nan
     
     selected_word = random.choice(candidate_tokens)
     inflections = getAllInflections(selected_word.lemma_, pos_type=selected_word.tag_[0])
 
-    unique_values = [selected_word.lower_]
+    unique_values = ['______', selected_word.lower_]
 
     # add the plural form for checking user's knowledge of noun pluralization
     if selected_word.pos_.startswith('N'):
@@ -51,17 +86,68 @@ def select_word(sentence):
         unique_values.pop()
 
     random.shuffle(unique_values)
-    return selected_word, unique_values
+    return selected_word.text, unique_values
 
-def missing_word(sentence):
+# TODO: remake the functionality of this function
+# def missing_word(sentence):
+#     """
+#     Selects a word from a given sentence and returns it 
+#     """
+#     doc = nlp(sentence)
+#     candidate_tokens = [token for token in doc if not token.is_punct and not token.is_space
+#                             and token.is_lower and token.lemma_ in ogden_basic_english]
+#     if not candidate_tokens:
+#         return np.nan
+    
+#     selected_word = random.choice(candidate_tokens)
+#     return selected_word
+
+def arrange_words(sentence):
     """
-    Selects a word from a given sentence and returns it 
+    Returns a list of shuffled words extracted from a sentence.
+    """
+    if not sentence:
+        return np.nan
+    doc = nlp(sentence)
+    if len(doc) > 15:
+        return np.nan
+    tokens = [token.text for token in doc if not token.is_space and not token.is_punct]
+    correct_sentence = ' '.join(tokens)
+    random.seed(123)
+    random.shuffle(tokens)
+    
+    # ! streamlit_sortables can't handle duplicates in a list
+    processed_tokens = []
+    for token in tokens:
+        if token in processed_tokens:
+            token += " "
+        processed_tokens.append(token)
+    return correct_sentence, processed_tokens
+
+def word_category(sentence):
+    """
+    Randomly selects a word from a given sentence and returns the chosen word and its category.
     """
     doc = nlp(sentence)
-    candidate_tokens = [token for token in doc if not token.is_punct and not token.is_space
-                            and token.is_lower and token.lemma_ in ogden_basic_english]
-    if not candidate_tokens:
+    
+    word_categories = {}
+    for token in doc:
+        for category, tags in categories.items():
+            if token.pos_ in tags:
+                if category not in word_categories:
+                    word_categories[category] = []
+                word_categories[category].append(token.text)
+                
+    if not word_categories:
         return np.nan
     
-    selected_word = random.choice(candidate_tokens)
-    return selected_word
+    category = random.choice(list(word_categories.keys()))
+    chosen_word = random.choice(word_categories[category])
+    
+    return category, chosen_word
+
+exercise_functions = {
+    'select_word': select_word,
+    'arrange_words': arrange_words,
+    'word_category': word_category
+}
